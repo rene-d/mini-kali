2d28d394db83737c561f4017adee780a3d686028
rene-d <rene-d@users.noreply.github.com>
2020-06-20 20:05:48 +0200

    arm use debian:testing

--- build.sh
+++ build.sh
@@ -5,11 +5,17 @@ IMAGE_NAME=${IMAGE_NAME:-kali}
 echo "build ${IMAGE_NAME} $1 ..."
 
 if [[ "$1" == "arm" ]]; then
-    docker buildx build --pull --platform linux/arm/v7 -t ${IMAGE_NAME}:arm --load --build-arg IMAGE_NAME=${IMAGE_NAME} .
+    docker buildx build --pull --platform linux/arm/v7 -t ${IMAGE_NAME}:arm --load --build-arg IMAGE_NAME=${IMAGE_NAME} -f Dockerfile-arm .
 elif [[ "$1" == "arm64" ]]; then
-    docker buildx build --pull --platform linux/arm64 -t ${IMAGE_NAME}:arm64 --load --build-arg IMAGE_NAME=${IMAGE_NAME} .
+    docker buildx build --pull --platform linux/arm64 -t ${IMAGE_NAME}:arm64 --load --build-arg IMAGE_NAME=${IMAGE_NAME} -f Dockerfile-arm .
 elif [[ "$1" == "crypto" ]]; then
     docker build --pull --tag ${IMAGE_NAME}:crypto -f Dockerfile-crypto --build-arg IMAGE_NAME=${IMAGE_NAME} .
+elif [[ "$1" == "all" ]]; then
+    export IMAGE_NAME
+    $0
+    $0 crypto
+    $0 arm
+    $0 arm64
 else
     docker build --pull --tag ${IMAGE_NAME} --build-arg IMAGE_NAME=${IMAGE_NAME} .
 fi
